fbd9470519d897cc2675ac4a1f4f037df45ec21b
jeffskwang <jeffskwang@gmail.com>
2019-10-29 14:09:34 -0400

    SOC_LEM with stratigraphy now!
    need to check, if the stratigraphy is correct. small scale tests needed.

--- SOC_LEM/SOC_LEM.py
+++ SOC_LEM/SOC_LEM.py
@@ -8,7 +8,7 @@ import time
 parent = os.getcwd()
 
 #run name
-runname='willis_test1'
+runname='willis_strat'
 
 #initial condition
 ini_file = 'willis_elev_test.asc'
@@ -17,17 +17,17 @@ ini_file = 'willis_elev_test.asc'
 U = 0.0            # [m/yr]  uplift
 K = 0.0001      # [1/yr] vertical erodibility constant
 D = 0.1            #[m^2/yr] hillslope diffusion coefficient
-La = 0.1           # [m] active layer
+La = 0.2           # [m] active layer
 K_SOC = 0.1  # [m] SOC exponent, i.e. SOC[z] = C_SOC * exp(-z/K_SOC)
 C_SOC = 0.05# [1/m] SOC coeffcieint, i.e. SOC[z] = C_SOC * exp(-z/K_SOC)
 
 #numerical parameters
-T = 150. # [yr] Simulation Time
+T = 300. # [yr] Simulation Time
 dt = 1.0 # [yr] model timestep
 hole_function = 0# 0 is off and 1 is on
 dz = 0.01 #[m] soil depth grid step
-nz = 100 #dz cells
-Z = 0.5 #max deposition, erosion
+nz = 200 #dz cells
+Z = 1.0 #max deposition, erosion
 
 #output parameters
 dt_plot = 10. # [yr] plot timestep
@@ -53,16 +53,21 @@ from landlab.io import read_esri_ascii
 nrows = grid.number_of_node_rows
 ncols = grid.number_of_node_columns
 #define arrays
-SOC_La = grid.add_ones('node','SOC_La')
-SOC_La *= 0.05
+SOC_La = grid.add_zeros('node','SOC_La')
 SOC_transfer = grid.add_zeros('node','SOC_transfer')
 SOC_z = np.zeros((nrows,ncols,nz))
 dz_ini = np.zeros((nrows,ncols,nz))
+
 for i in range(0,nrows):
     for j in range(0,ncols):
         dz_ini[i,j,:] = np.linspace(-Z + dz / 2.0, Z - dz / 2.0, nz)
-        SOC_z[i,j,0:50] = C_SOC * np.exp(dz_ini[i,j,0:50] / K_SOC)
-
+        SOC_z[i,j,0:int(nz/2)] = C_SOC * np.exp(dz_ini[i,j,0:int(nz/2)] / K_SOC)
+        avg_count = 0
+        for k in range(int(nz/2 - La / dz),int(nz/2)):
+            avg_count += 1
+            SOC_La.reshape(nrows,ncols)[i,j] += SOC_z[i,j,k]
+        SOC_La.reshape(nrows,ncols)[i,j] /= float(avg_count)
+            
 #grid size and number of cells
 dx = grid.dx
 dy = grid.dy
@@ -106,13 +111,21 @@ def soil_and_SOC_transport(eta,SOC_La):
 def find_SOC_cell(interface_z,z_coor,SOC):
     index_locat = (np.abs(z_coor - interface_z)).argmin()
     SOC_interface = SOC[index_locat]
-    return SOC_interface        
+    
+    return SOC_interface  
 
-def SOC_transfer_function(eta_old,eta_ini,dzdt,SOC_La,SOC_transfer):
+def find_top_cell_active_layer(interface_z,z_coor,SOC):
+    top_cell_active_layer = (np.abs(z_coor - interface_z + dz /2.)).argmin()
+    
+    return top_cell_active_layer        
+
+def find_bottom_cell_active_layer(interface_z,z_coor,SOC):
+    bottom_cell_active_layer = (np.abs(z_coor - interface_z)).argmin()
+    
+    return bottom_cell_active_layer      
+
+def SOC_transfer_function(eta_old,eta_ini,dzdt,SOC_La,SOC_transfer,SOC_z):
     interface = eta_old - eta_ini - La
-##    SOC_transfer[dzdt>0.0] = SOC_La[dzdt>0.0] 
-##    SOC_transfer[dzdt<0.0] = C_SOC * np.exp(interface[dzdt<0.0] / K_SOC)
-
     for i in range(0,nrows):
         for j in range(0,ncols):
             if dzdt.reshape(nrows,ncols)[i,j] < 0.0:
@@ -122,10 +135,25 @@ def SOC_transfer_function(eta_old,eta_ini,dzdt,SOC_La,SOC_transfer):
                 
     return SOC_transfer
 
-#####################
+def SOC_profile_update(eta,eta_ini,dzdt,SOC_La,SOC_z):
+    interface = eta - eta_ini - La
+    for i in range(0,nrows):
+        for j in range(0,ncols):
+            top_cell_active_layer =  find_top_cell_active_layer(interface.reshape(nrows,ncols)[i,j]+La,dz_ini[i,j,:],SOC_z[i,j,:])
+            bottom_cell_active_layer =  find_bottom_cell_active_layer(interface.reshape(nrows,ncols)[i,j],dz_ini[i,j,:],SOC_z[i,j,:])
+            SOC_z[i,j,top_cell_active_layer+1:] = 0.0
+            SOC_z[i,j,bottom_cell_active_layer+1:top_cell_active_layer+1]=SOC_La.reshape(nrows,ncols)[i,j]
 
+            if dzdt.reshape(nrows,ncols)[i,j] > 0.0:
+                dz_interface_old = (interface.reshape(nrows,ncols)[i,j] - dt * dzdt.reshape(nrows,ncols)[i,j]) - dz_ini[i,j,bottom_cell_active_layer]  + dz / 2.0
+                dz_interface_new = interface.reshape(nrows,ncols)[i,j] - dz_ini[i,j,bottom_cell_active_layer]  + dz / 2.0
+                SOC_z[i,j,bottom_cell_active_layer] = (SOC_z[i,j,bottom_cell_active_layer] * dz_interface_old + dt * dzdt.reshape(nrows,ncols)[i,j] *  SOC_La.reshape(nrows,ncols)[i,j]) / (dz_interface_new)
+                
+    return SOC_z
+
+
+##### LOOP START #####
 eta_ini = eta.copy()
-### LOOP START ###
 for t in range(0,nt + 1):
     if t%nt_plot == 0:
         print ('Time = ' + str(t * dt) + ' yrs; ' + str(int(float(t)*dt/T*1000.)/10.) + '% done')
@@ -145,9 +173,11 @@ for t in range(0,nt + 1):
     dqda,dqcda = soil_and_SOC_transport(eta,SOC_La)
     eta[grid.core_nodes] += dt *(U - dqda[grid.core_nodes])
     dzdt = (eta - eta_old)/dt
-    SOC_transfer_function(eta_old,eta_ini,dzdt,SOC_La,SOC_transfer)
+    SOC_transfer = SOC_transfer_function(eta_old,eta_ini,dzdt,SOC_La,SOC_transfer,SOC_z)
     SOC_La[grid.core_nodes]  += dt/La * (SOC_transfer[grid.core_nodes] * dqda[grid.core_nodes]  - dqcda[grid.core_nodes] )
+    SOC_z  = SOC_profile_update(eta,eta_ini,dzdt,SOC_La,SOC_z)
 
+np.save('final_SOC.npy',SOC_z)
 #end time
 stop_time = time.time()
 print (str(round((stop_time -start_time )/60.,1))+' mins')
